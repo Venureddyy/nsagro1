d7c59fde0eaa13a59b36dd2c263cfd4331476019
Venureddyy <59521333+Venureddyy@users.noreply.github.com>
2024-12-23 18:27:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript for NS AGRO AGENCY Website
+
+// Navigation functionality
+function navigateTo(page) {
+    window.location.href = page;
+}
+
+// Additional interactivity can be added here
